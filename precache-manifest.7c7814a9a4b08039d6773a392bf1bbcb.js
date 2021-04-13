5dbfe7795eb4118ba1c5c31e8b58251a42c5715f
elisagc <elisagcubero@gmail.com>
2021-04-13 12:42:22 +0200

    Updates

---
+++ precache-manifest.7c7814a9a4b08039d6773a392bf1bbcb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b34020c5444312fb9b665851d90b94c",
+    "url": "/decathlon-challenge/index.html"
+  },
+  {
+    "revision": "78ad7df2de51eed5b607",
+    "url": "/decathlon-challenge/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "2b71bd6675e5f77240d2",
+    "url": "/decathlon-challenge/static/css/main.0b07e331.chunk.css"
+  },
+  {
+    "revision": "78ad7df2de51eed5b607",
+    "url": "/decathlon-challenge/static/js/2.b5c5e693.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/decathlon-challenge/static/js/2.b5c5e693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b71bd6675e5f77240d2",
+    "url": "/decathlon-challenge/static/js/main.88122625.chunk.js"
+  },
+  {
+    "revision": "deeb68d54d9ca3a37afb",
+    "url": "/decathlon-challenge/static/js/runtime-main.75824c01.js"
+  },
+  {
+    "revision": "4829f91294f35715c8f80d251fbebab3",
+    "url": "/decathlon-challenge/static/media/imagen1.4829f912.jpg"
+  },
+  {
+    "revision": "e7c0ecedcfcbea8028a023faa3512b1d",
+    "url": "/decathlon-challenge/static/media/imagen2.e7c0eced.jpg"
+  },
+  {
+    "revision": "63b11c4cad736365b9de90f3ec264cb9",
+    "url": "/decathlon-challenge/static/media/imagen3.63b11c4c.jpg"
+  },
+  {
+    "revision": "5273d30da902f4edef470ae45a2d4f67",
+    "url": "/decathlon-challenge/static/media/imagen4.5273d30d.jpg"
+  },
+  {
+    "revision": "9ef36ac3651d8c2cecf0544c3abefd98",
+    "url": "/decathlon-challenge/static/media/imagen5.9ef36ac3.jpg"
+  },
+  {
+    "revision": "e11aa483df3b9b9810aa529d54d96c01",
+    "url": "/decathlon-challenge/static/media/imagen6.e11aa483.jpg"
+  },
+  {
+    "revision": "55f8a91d377f2cb68f3e82e28065cc95",
+    "url": "/decathlon-challenge/static/media/slider_principal.55f8a91d.jpg"
+  }
+]);
